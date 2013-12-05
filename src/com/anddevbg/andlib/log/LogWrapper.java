02088dc6e32546acc03c13c8de7da0fdcc56daa8
anddevbg <anddevbg@gmail.com>
2013-12-06 00:13:27 +0200

    Added init method to log wrapper.

--- src/com/anddevbg/andlib/log/LogWrapper.java
+++ src/com/anddevbg/andlib/log/LogWrapper.java
@@ -1,27 +1,24 @@
 package com.anddevbg.andlib.log;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.text.TextUtils;
 import android.util.Log;
 
 /**
  * 
  * @author anddevbg@gmail.com
- *
+ * 
  */
 public class LogWrapper {
 
-	public static boolean SHOW_LOGS = false;
-	
+	private static boolean SHOW_LOGS = false;
+
 	private LogWrapper() {
 
 	}
-	
-	public static void disableLogs() {
-		SHOW_LOGS = false;
-	}
-	
-	public static void enableLogs() {
-		SHOW_LOGS = true;
+	public static void initLogs(Context context) {
+		SHOW_LOGS = (0 != (context.getApplicationInfo().flags &= ApplicationInfo.FLAG_DEBUGGABLE));
 	}
 	
 	public static void d(Object tag, String message) {
